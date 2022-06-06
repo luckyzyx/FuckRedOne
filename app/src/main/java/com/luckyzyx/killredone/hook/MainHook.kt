40ac3793bad67e20eae2d59f0a53357f46bea9aa
luckyzyx <2813467705@qq.com>
2022-06-06 11:15:27 +0800

    modify packagename

--- app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
+++ app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
@@ -1,4 +1,4 @@
-package com.luckyzyx.fuckredone.hook
+package com.luckyzyx.killredone.hook
 
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
