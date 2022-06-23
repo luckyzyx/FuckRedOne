ec94bab32e013411a8e77b6824195f997ab5307f
luckyzyx <2813467705@qq.com>
2022-06-23 17:24:49 +0800

    Adapted to 9RT C03 desktop clock component

--- app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
+++ app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
@@ -68,7 +68,8 @@ class MainHook : IYukiHookXposedInit {
             "com.coloros.widget.smallweather.OnePlusWidget".clazz.field {
                 name {
                     equalsOf(other = "Sb",isIgnoreCase = false)//9RT->C12
-                    equalsOf(other = "Sc",isIgnoreCase = false)//9RT->C12.1
+                    equalsOf(other = "Sc",isIgnoreCase = false)//9RT->C12.1 C02
+                    equalsOf(other = "TR",isIgnoreCase = false)//9RT->C12.1 C03
                 }
                 type(CharSequenceType)
             }.get().set("")
