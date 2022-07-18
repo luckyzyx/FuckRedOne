7fdb70bed230b852441c47bcf13b884986882c7e
luckyzyx <2813467705@qq.com>
2022-07-18 18:44:35 +0800

    Update the version of the dependency library to adapt to the 9RT C04 desktop clock component (Clock_12.0.76 --> c05c8c0)

--- app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
+++ app/src/main/java/com/luckyzyx/killredone/hook/MainHook.kt
@@ -13,7 +13,7 @@ import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 class MainHook : IYukiHookXposedInit {
     override fun onInit() {
         configs {
-            debugTag = "FuckRedOne"
+            debugTag = "KillRedOne"
             isDebug = false
         }
     }
@@ -70,6 +70,7 @@ class MainHook : IYukiHookXposedInit {
                     equalsOf(other = "Sb",isIgnoreCase = false)//9RT->C12
                     equalsOf(other = "Sc",isIgnoreCase = false)//9RT->C12.1 C02
                     equalsOf(other = "TR",isIgnoreCase = false)//9RT->C12.1 C03
+                    equalsOf(other = "TT",isIgnoreCase = false)//9RT->C12.1 C04
                 }
                 type(CharSequenceType)
             }.get().set("")
